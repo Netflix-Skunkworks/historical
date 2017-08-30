5922076bcd06130a1c39382e5b6c08fef8fa64fe
Mike Grima <mgrima@netflix.com>
2017-08-30 11:19:42 -0700

    Added fixtures for use in more tests.

--- historical/tests/conftest.py
+++ historical/tests/conftest.py
@@ -1,5 +1,125 @@
 import pytest
 from moto.dynamodb2 import mock_dynamodb2
+from moto.kinesis import mock_kinesis
+from moto.s3 import mock_s3
+from moto.iam import mock_iam
+from moto.sts import mock_sts
+import boto3
+import os
+
+
+@pytest.fixture(scope="function")
+def s3():
+    with mock_s3():
+        yield boto3.client("s3")
+
+
+@pytest.fixture(scope="function")
+def sts():
+    with mock_sts():
+        yield boto3.client("sts")
+
+
+@pytest.fixture(scope="function")
+def iam():
+    with mock_iam():
+        yield boto3.client("iam")
+
+
+@pytest.fixture(scope="function")
+def dynamodb():
+    with mock_dynamodb2():
+        yield boto3.client("dynamodb", region_name="us-east-1")
+
+
+@pytest.fixture(scope="function")
+def swag_table(dynamodb):
+    resource = boto3.resource('dynamodb', region_name='us-east-1')
+
+    table = resource.create_table(
+        TableName='accounts',
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        })
+
+    table.meta.client.get_waiter('table_exists').wait(TableName='accounts')
+
+
+@pytest.fixture(scope="function")
+def swag_accounts(swag_table):
+    from swag_client.backend import SWAGManager
+    from swag_client.util import parse_swag_config_options
+
+    swag_opts = {
+        'swag.type': 'dynamodb',
+        'swag.namespace': 'accounts',
+        'swag.cache_expires': 0
+    }
+
+    swag = SWAGManager(**parse_swag_config_options(swag_opts))
+
+    account = {
+        'aliases': ['test'],
+        'contacts': ['admins@test.net'],
+        'description': 'LOL, Test account',
+        'email': 'testaccount@test.net',
+        'environment': 'test',
+        'id': '012345678910',
+        'name': 'testaccount',
+        'owner': 'wouldntyouliketoknow',
+        'provider': 'aws',
+        'sensitive': False
+    }
+
+    os.environ['SWAG_ENABLED'] = "True"
+
+    swag.create(account)
+
+
+@pytest.fixture(scope="function")
+def historical_role(iam, sts):
+    iam.create_role(RoleName="historicalrole", AssumeRolePolicyDocument="{}")
+    os.environ["HISTORICAL_ROLE"] = "historicalrole"
+
+
+@pytest.fixture(scope="function")
+def historical_kinesis():
+    with mock_kinesis():
+        client = boto3.client("kinesis", region_name="us-east-1")
+        client.create_stream(StreamName="historicalstream", ShardCount=1)
+        os.environ["HISTORICAL_STREAM"] = "historicalstream"
+
+        yield client
+
+
+@pytest.fixture(scope="function")
+def buckets(s3):
+    # Create buckets:
+    for i in range(0, 50):
+        s3.create_bucket(Bucket="testbucket{}".format(i))
+
+
+@pytest.fixture(scope="function")
+def mock_lambda_context():
+    class MockLambdaContext():
+        @staticmethod
+        def get_remaining_time_in_millis():
+            return 9999999999
+
+    return MockLambdaContext()
 
 
 @pytest.fixture()
